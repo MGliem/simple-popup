cbc10cecdb14f1e668bfa9ac3ec42a11809899dc
MGliem <maximilian.gliem@gmail.com>
2022-11-08 20:36:47 +0100

    created my popup

---
+++ src/components/Popup.tsx
@@ -0,0 +1,21 @@
+const Popup = ({ setShowPopup }: { setShowPopup: Function }) => {
+  return (
+    <div
+      className="absolute top-0 left-0 w-full h-screen bg-slate-500/[0.8] flex justify-center items-center"
+      onClick={() => setShowPopup(false)}
+    >
+      <div className="w-fit h-[30vh] p-5 bg-slate-300 rounded-xl flex flex-col text-black">
+        <h1 className="mb-5">Ma popup</h1>
+        <p className="flex-grow">Du text qui dit ce que fait ma popup</p>
+        <button
+          className="w-fit mx-auto bg-lime-500"
+          onClick={() => setShowPopup(false)}
+        >
+          Ok
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
