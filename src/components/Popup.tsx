3062428d7d983ef3efd774bf64ea3681311ffab1
MGliem <maximilian.gliem@gmail.com>
2022-11-09 02:02:31 +0100

    fixed if already scrolled down

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -12,7 +12,7 @@ const Popup = ({ setShowPopup }: { setShowPopup: Function }) => {
   }, []);
 
   return (
-    <div className="absolute top-0 left-0 w-full h-screen bg-slate-500/[0.8] flex justify-center items-center">
+    <div className="fixed top-0 left-0 w-full h-screen bg-slate-500/[0.8] flex justify-center items-center">
       <div className="w-fit min-h-[30vh] p-5 bg-slate-300 rounded-xl flex flex-col text-black transition-popup">
         <h1 className="mb-5">Ma popup</h1>
         <p className="flex-grow">Du text qui dit ce que fait ma popup</p>
