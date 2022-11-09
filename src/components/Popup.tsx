2c5543785d1fa6e720043c1e839baecdb56498b1
MGliem <maximilian.gliem@gmail.com>
2022-11-09 01:45:39 +0100

    add animation on popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,7 +1,9 @@
+import "./popup.css";
+
 const Popup = ({ setShowPopup }: { setShowPopup: Function }) => {
   return (
     <div className="absolute top-0 left-0 w-full h-screen bg-slate-500/[0.8] flex justify-center items-center">
-      <div className="w-fit h-[30vh] p-5 bg-slate-300 rounded-xl flex flex-col text-black">
+      <div className="w-fit h-[30vh] p-5 bg-slate-300 rounded-xl flex flex-col text-black transition-popup">
         <h1 className="mb-5">Ma popup</h1>
         <p className="flex-grow">Du text qui dit ce que fait ma popup</p>
         <button
