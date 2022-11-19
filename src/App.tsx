c0745e8ad3779fce0c7da2591a0968efca413bda
MGliem <maximilian.gliem@gmail.com>
2022-11-19 17:54:07 +0100

    using react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Popup from "./components/Popup";
 import reactLogo from "./assets/react.svg";
-
+import { Helmet } from "react-helmet"
 import "./App.css";
 
 function App() {
@@ -10,6 +10,9 @@ function App() {
 
   return (
     <div className="App">
+      <Helmet>
+        {showPopup ? <body className="overflow-hidden" /> : <body className="overflow-auto" />}
+      </Helmet>
       <div className="flex flex-col items-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
